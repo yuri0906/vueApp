af0bb4c887423efdcb88e280045d0282357f4640
takatsukayuriko <takayuri0906@gmail.com>
2020-10-02 16:35:38 +0900

    apiid別ファイルへ移動

--- backend/index.js
+++ backend/index.js
@@ -8,13 +8,13 @@ app.use(cors())
 
 const axios = require("axios");
 const url = "https://app.rakuten.co.jp/services/api/IchibaItem/Search/20170706?";
-const api = require("./password/apiID");
+const api = require("./password/apiID"); //apiID取得用ファイル
 
 
 app.post('/',function(req, res) {
   axios.get(url,{
     params:{ //paramを追加
-      "applicationId" : api.id,
+      "applicationId" : api.id, //楽天APIのIDを/password/apiIDから取得
       "keyword" : req.body.text //キーワードを取得
     }
   })
