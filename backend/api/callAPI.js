8914b0de02d3f1490683c0a4e27cafe0d8e5e9df
takatsukayuriko <takayuri0906@gmail.com>
2020-10-06 17:07:04 +0900

    APIURL別ファイルに移動

--- backend/api/callAPI.js
+++ backend/api/callAPI.js
@@ -3,14 +3,13 @@ API呼び出し処理実装
 */
 
 const axios = require("axios"); 
-const rakuten_url = "https://app.rakuten.co.jp/services/api/IchibaItem/Search/20170706?"; //楽天APIのURL
-const yahoo_url = "https://jlp.yahooapis.jp/KeyphraseService/V1/extract?"; //YahooAPIのURL
+const apiURL = require("../password/url");
 const apiKey = require("../password/apiID"); //apiID取得用ファイル
 
 //商品を検索し、その説明文(itemCaptionを抽出する)
 exports.searchProduct = async(keyword) => { 
     const captionList = new Array(); //各商品の説明文を含む配列
-    const result = await axios.get(rakuten_url,{
+    const result = await axios.get(apiURL.rakuten,{
         params:{ //paramを追加
           "applicationId" : apiKey.rakuten, //楽天APIのID取得
           "keyword" : keyword //キーワードを取得
@@ -29,7 +28,7 @@ exports.searchProduct = async(keyword) => {
 
 //与えられたテキスト(商品説明)中のキーフレーズを抽出する
 exports.extractKeyphrase = async(sentence) => {
-    const result = await axios.get(yahoo_url,{
+    const result = await axios.get(apiURL.yahoo,{
         params:{ //paramを追加
           "appid" : apiKey.yahoo, //YahooAPIのID取得
           "sentence" : sentence,
