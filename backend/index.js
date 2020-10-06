d79dcb7e584bf9cd93782391c04cce2142c1f77e
takatsukayuriko <takayuri0906@gmail.com>
2020-10-06 11:16:15 +0900

    backend/apiに関数分割ファイル3つ作成

--- backend/index.js
+++ backend/index.js
@@ -8,13 +8,13 @@ app.use(cors())
 
 const axios = require("axios");
 const url = "https://app.rakuten.co.jp/services/api/IchibaItem/Search/20170706?";
-const api = require("./password/apiID"); //apiID取得用ファイル
+const apiKey = require("./password/apiID"); //apiID取得用ファイル
 
 
 app.post('/',function(req, res) {
   axios.get(url,{
     params:{ //paramを追加
-      "applicationId" : api.id, //楽天APIのIDを/password/apiIDから取得
+      "applicationId" : apiKey.rakuten, //楽天APIのID取得
       "keyword" : req.body.text //キーワードを取得
     }
   })
--- frontend/src/components/Search.vue
+++ frontend/src/components/Search.vue
@@ -4,6 +4,7 @@
       <p>検索したい商品を入力してください</p>
       <input type="text" v-model="textInput"/>
       <input type="submit" value="検索" @click="showResult"/>
+      <p>検索結果?件</p>
       <p>{{ result }}</p>
   </div>
 </template>
@@ -21,6 +22,10 @@ export default class Search extends Vue {
   サーバーへリクエスト送信、レスポンス（API処理結果）を受け取る
   */
   async showResult(){
+   //検索件数(count)
+   //商品名(Items.Item.itemName)
+   //for文で各商品表示
+   //JSON書式に変更？
    const response = await Methods.sendParams(this.textInput);
    this.result = response.data;
   }
