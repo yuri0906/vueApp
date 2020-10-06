9ec3a079ec033c8db7ca1a6fcddbe941fbe8f0c4
takatsukayuriko <takayuri0906@gmail.com>
2020-10-06 11:43:17 +0900

    API処理ファイル実装

--- backend/api/searchProduct.js
+++ backend/api/searchProduct.js
@@ -1,3 +1,22 @@
 /*
-楽天商品検索API呼び出し処理実装
+楽天商品検索API呼び出し処理実装（未テスト）
 */
+const axios = require("axios");
+const url = "https://app.rakuten.co.jp/services/api/IchibaItem/Search/20170706?";
+const apiKey = require("../password/apiID"); //apiID取得用ファイル
+
+//商品を検索する
+exports.searchProduct = async(keyword) => {
+    await axios.get(url,{
+        params:{ //paramを追加
+          "applicationId" : apiKey.rakuten, //楽天APIのID取得
+          "keyword" : keyword //キーワードを取得
+        }
+      })
+        .then((response) => {
+          return response;
+        })
+        .catch((e) => {
+          return e;
+        });
+}
