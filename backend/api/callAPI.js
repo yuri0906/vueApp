b44dc4d8b2d1e1f2b1d6f892ed8d2d7c5e11871c
takatsukayuriko <takayuri0906@gmail.com>
2020-10-07 11:09:36 +0900

    インデント修正

--- backend/api/callAPI.js
+++ backend/api/callAPI.js
@@ -8,44 +8,49 @@ const apiKey = require("../secret/apiID"); //apiID取得用ファイル
 
 //商品を検索し、その説明文(itemCaptionを抽出する)
 exports.searchProduct = async(keyword) => { 
-    const captionList = new Array(); //各商品の説明文を含む配列
-    const result = await axios.get(apiURL.rakuten,{
-        params:{ //paramを追加
-          "applicationId" : apiKey.rakuten, //楽天APIのID取得
-          "keyword" : keyword //キーワードを取得
-        }
-      }).catch((err) => {
-        console.log('error occured in rakuten api!!!');
-        return; 
-      });
+  const captionList = new Array(); //各商品の説明文を含む配列
+  const result = await axios.get(apiURL.rakuten,{
+      params:{ //paramを追加
+        "applicationId" : apiKey.rakuten, //楽天APIのID取得
+        "keyword" : keyword //キーワードを取得
+      }
+    }).catch((err) => {
+      console.log('error occured in rakuten api!!!');
+      return; 
+    });
 
-    const items = result.data["Items"]; 
-    for(let i in items){
-        captionList[i] = items[i].Item["itemCaption"]; //各商品の説明文を配列に追加
-    }
-    return captionList;
+  const items = result.data["Items"]; 
+  for(let i in items){
+      captionList[i] = items[i].Item["itemCaption"]; //各商品の説明文を配列に追加
+  }
+  return captionList;
 }
 
 //与えられたテキスト(商品説明)中のキーフレーズを抽出する
 exports.extractKeyphrase = async(sentence) => {
-    const result = await axios.get(apiURL.yahoo,{
-        params:{ //paramを追加
-          "appid" : apiKey.yahoo, //YahooAPIのID取得
-          "sentence" : sentence, //商品説明欄
-          "output" : "json" //レスポンスはJSON形式を選択
-        }
-      }).catch((err) => {
-        console.log('error occured in yahoo api!!!');
-        return; 
-      });
-    return result.data;
+  const result = await axios.get(apiURL.yahoo,{
+    params:{ //paramを追加
+      "appid" : apiKey.yahoo, //YahooAPIのID取得
+      "sentence" : sentence, //商品説明
+      "output" : "json" //レスポンスはJSON形式を選択
+    }
+  }).catch((err) => {
+    console.log('error occured in yahoo api!!!');
+    return; 
+  });
+  return result.data;
 }
 
 /*
 データ集計（未実装）
 */
-
+exports.totalization = async(objlist) => {
+  return "";
+}
 
 /*
 グラフ化処理（未実装）
 */
+exports.visualization = async(objlist) => {
+  return "";
+}
