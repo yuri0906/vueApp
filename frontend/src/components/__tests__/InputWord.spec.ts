acd6496ab9e4cfd258591c36cbafd007f828eb6c
takatsukayuriko <takayuri0906@gmail.com>
2020-11-11 17:05:58 +0900

    テストプログラム一部修正

--- frontend/src/components/__tests__/InputWord.spec.ts
+++ frontend/src/components/__tests__/InputWord.spec.ts
@@ -1,18 +1,10 @@
 import { shallowMount } from '@vue/test-utils'
 import InputWord from '../InputWord.vue'
 
-describe('ランキング表示テスト',()=>{
+describe('入力フォーム部分テスト',()=>{
     it('入力フォームが描画される', () => {
         const wrapper = shallowMount(InputWord);
         expect(wrapper.find('.input-word').exists()).toBe(true)
     })
-    /*
-    it('データをセットすると、ランキングが描画される', () => {
-        const wrapper = shallowMount(InputWord,{
-            propsData: {
-                
-            }
-        });
-        expect(wrapper.find('.word-ranking').exists()).toBe(true)
-    })*/
+
 })
