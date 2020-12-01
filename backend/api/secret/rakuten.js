fc334ae466b7032b19ba7cb0ffa970aa21f5d9b3
takatsukayuriko <takayuri0906@gmail.com>
2020-12-01 17:00:12 +0900

    gitignore追加

--- backend/api/secret/rakuten.js
+++
@@ -1,2 +0,0 @@
-exports.id = "1076369533716180220"; 
-exports.url = "https://app.rakuten.co.jp/services/api/IchibaItem/Search/20170706?"; 
--- backend/api/secret/yahoo.js
+++
@@ -1,2 +0,0 @@
-exports.id = "dj00aiZpPVpsc2lUSHk3a3hDRyZzPWNvbnN1bWVyc2VjcmV0Jng9MjA-"; 
-exports.url = "https://jlp.yahooapis.jp/KeyphraseService/V1/extract?"; 
